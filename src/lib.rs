f55c58f619f495fc2e4ea35edde25448b00d06d5
TatriX <tatrics@gmail.com>
2019-07-03 12:02:30 +0200

    feat: Implement new protocol and basic rendering

--- src/lib.rs
+++ src/lib.rs
@@ -7,9 +7,9 @@
 
 #[macro_use]
 extern crate seed;
-use seed::{prelude::*, App};
-use wasm_bindgen::JsCast;
+use seed::prelude::*;
 use serde::{Deserialize, Serialize};
+use wasm_bindgen::JsCast;
 use web_sys::{MessageEvent, WebSocket};
 
 const WS_URL: &str = "ws://34.68.64.169:8080";
@@ -17,124 +17,181 @@ const WS_URL: &str = "ws://34.68.64.169:8080";
 #[wasm_bindgen]
 pub fn render() {
     seed::App::build(Model::new(30), update, view)
-    // `trigger_update_handler` is necessary,
-    // because we want to process `seed::update(..)` calls.
+        // `trigger_update_handler` is necessary,
+        // because we want to process `seed::update(..)` calls.
         .window_events(|_| vec![trigger_update_handler()])
         .finish()
         .run();
 }
 
-
 #[derive(Clone)]
 struct Model {
+    ws: WebSocket,
     size: u32,
     connected: bool,
-    ws: WebSocket,
+    session: String,
+    history: History,
 }
 
 impl Model {
     fn new(size: u32) -> Self {
         let ws = WebSocket::new(WS_URL).unwrap();
         register_handlers(&ws);
-        Self{
-            size,
-            connected: false,
+        Self {
             ws,
+            size,
+            session: "debug".into(),
+            connected: false,
+            history: History::default(),
         }
     }
+
+    /// Create frame ready to be sent over the websocket with the
+    /// current session id.
+    fn frame(&self, message: impl Serialize) -> String {
+        let frame = (&self.session, message);
+        serde_json::to_string(&frame).expect("cannot create frame")
+    }
+
+    fn send(&self, message: impl Serialize) {
+        self.ws
+            .send_with_str(&self.frame(message))
+            .expect("cannot send message");
+    }
 }
-
-
 
 // `Serialize` is required by `seed::update(..)`
 // `Deserialize` is required by `trigger_update_handler`
 #[derive(Clone, Serialize, Deserialize)]
 enum Msg {
-    Connect{
-        session: String,
-        player_name: String,
-    },
+    Connect { player_name: String },
     Connected,
     ServerMessage(ServerMessage),
-    Dummy,
+    Move { x: u32, y: u32 },
 }
-
 
 /// Message from the server to the client.
 #[derive(Debug, Clone, Serialize, Deserialize)]
-struct ServerMessage {
-    pub id: usize,
-    pub text: String,
+#[serde(tag = "message", content = "data")]
+enum ServerMessage {
+    Connected {
+        #[serde(rename = "Player")]
+        player_name: String,
+    },
+    SetHistory {
+        #[serde(rename = "History")]
+        history: History,
+    },
+}
+
+#[derive(Debug, Default, Clone, Serialize, Deserialize)]
+struct History {
+    moves: Vec<Cell>,
+    players: Vec<String>,
 }
 
 /// Message from the client to the server.
-#[derive(Clone, Serialize, Deserialize)]
-#[serde(tag = "tag", content = "contents")]
+#[derive(Serialize)]
+#[serde(tag = "method", content = "resource")]
 enum ClientMessage {
-    Post {
-    },
     Connect {
-        tag: &'static str,
-        contents: String,
+        #[serde(rename = "Player")]
+        player_name: String,
     },
-    Get {
+    PostMove {
+        #[serde(rename = "Cell")]
+        cell: Cell,
     },
-    Delete {
-    },
+    GetHistory,
+    CleanHistory,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+struct Cell {
+    coord: Coord,
+    value: String,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+struct Coord {
+    row: u32,
+    col: u32,
 }
 
 fn update(msg: Msg, mut model: &mut Model, orders: &mut Orders<Msg>) {
     match msg {
-        Msg::Dummy => {
-            log!("Dummy!");
-        },
-        Msg::Connect{session, player_name} => {
-            let s = serde_json::to_string(&ClientMessage::Connect{
-                tag: "Player",
-                contents: player_name,
-            }).unwrap();
-            model.ws.send_with_str(&format!("[\"{}\", {}]", session, s)).unwrap();
-        },
+        Msg::Move { x, y } => {
+            model.send(ClientMessage::PostMove {
+                cell: Cell {
+                    coord: Coord { row: y, col: x },
+                    value: "X".into(),
+                },
+            });
+        }
+        Msg::Connect { player_name } => {
+            model.send(ClientMessage::Connect { player_name });
+            // this method also fetches history, the same way as original client does
+            model.send(ClientMessage::GetHistory)
+        }
         Msg::Connected => {
             log!("Connected!");
 
             model.connected = true;
-            orders.send_msg(Msg::Connect{
-                session: "penis".into(),
+            orders.send_msg(Msg::Connect {
                 player_name: "Vasya".into(),
             });
         }
-        Msg::ServerMessage(msg) => {
-            model.connected = true;
-            log!("Got {:?}", msg);
-        }
+        Msg::ServerMessage(msg) => match msg {
+            ServerMessage::Connected { player_name } => {
+                log!(player_name, "connected");
+                model.connected = true;
+            }
+            ServerMessage::SetHistory { history } => {
+                model.history = history;
+            }
+        },
     }
 }
 
 /// Main view
 fn view(model: &Model) -> Vec<El<Msg>> {
     vec![
-        h6!["PlayWithMe Rust/Seed edition!", simple_ev(Ev::Click, Msg::Dummy)],
+        h6!["PlayWithMe Rust/Seed edition!"],
         if model.connected {
-            div![draw_grid(model.size)]
+            div![draw_grid(model.size, &model.history)]
         } else {
             div!["Connecting..."]
-        }
+        },
     ]
 }
 
-fn draw_grid(size: u32) -> El<Msg> {
+fn draw_grid(size: u32, history: &History) -> El<Msg> {
     div![
         class!["grid"],
-        (0..size).map(|y| draw_row(size, y)).collect::<Vec<_>>()
+        (0..size)
+            .map(|y| draw_row(size, y, history))
+            .collect::<Vec<_>>()
     ]
 }
 
-fn draw_row(size: u32, y: u32) -> El<Msg> {
+fn draw_row(size: u32, y: u32, history: &History) -> El<Msg> {
     div![
         class!["row"],
         (0..size)
-            .map(|x| div![id!(&format!("cell-{}-{}", x, y)), class!["cell"]])
+            .map(|x| {
+                let content = history
+                    .moves
+                    .iter()
+                    .find(|cell| cell.coord.row == y && cell.coord.col == x)
+                    .map(|cell| cell.value.clone())
+                    .unwrap_or(" ".into());
+                div![
+                    id!(&format!("cell-{}-{}", x, y)),
+                    class!["cell"],
+                    simple_ev(Ev::Click, Msg::Move { x, y }),
+                    content,
+                ]
+            })
             .collect::<Vec<_>>()
     ]
 }
@@ -172,7 +229,7 @@ fn register_handler_on_message(ws: &web_sys::WebSocket) {
         log!("Client received a message");
         let txt = ev.data().as_string().unwrap();
         let json: ServerMessage = serde_json::from_str(&txt).unwrap();
-        log!("- text message: ", &txt);
+        log!(&txt);
         seed::update(Msg::ServerMessage(json));
     }) as Box<FnMut(MessageEvent)>);
 
